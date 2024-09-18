be78081174f6d6d67f9d065d007fa060d3c1fafb
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-18 23:21:38 +0700

    18/09/2024

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel as UICarousel,
@@ -31,7 +31,7 @@ const CustomCarousel = () => {
                       <CardContent className="p-0">
                         {" "}
                         {/* Remove padding */}
-                        <Image
+                        <img
                           src={imageSrc}
                           alt={`Slide ${index + 1}`}
                           className="w-full h-[350px] object-cover rounded-xl"
@@ -47,12 +47,12 @@ const CustomCarousel = () => {
           </UICarousel>
         </div>
         <div className="">
-          <Image
+          <img
             src="https://bds123.cdn.static123.com/images/thumbs/900x600/fit/2024/06/29/f03359771c88aa9e49bd5ac477ed7260_1719624650.jpg"
             alt=""
             className="w-[400px] h-[150px] ml-28 object-cover rounded-xl"
           />
-          <Image
+          <img
             src="https://bds123.cdn.static123.com/images/thumbs/900x600/fit/2023/09/27/70_1695787728.jpg"
             alt=""
             className="w-[400px] h-[200px] ml-28 mt-2 rounded-xl"
