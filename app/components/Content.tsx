71d79c1a71061f37276e8df334bf0adc3aeca016
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-18 22:05:18 +0700

    18/09/2024

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -4,7 +4,7 @@ const Content = () => {
   return (
     <div className="mt-5">
       <div className="flex justify-start items-center ml-32">
-        <div className="bg-red-500 text-white font-semibold py-2 px-4 w-48 transform -skew-x-6">
+        <div className="bg-red-500 text-white font-semibold py-2 px-4 w-48 transform  -skew-x-12">
           Phòng trọ giá rẻ
         </div>
       </div>
