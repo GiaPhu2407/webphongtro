6f358847c0fb4313f88bc4164cd6fbc83ad9045f
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-19 21:23:59 +0700

    19/09/2024

--- app/ShowLogin/page.tsx
+++ app/ShowLogin/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "../components/Login/Header";
+import FormLogin from "../components/Login/FormLogin";
 const page = () => {
   return (
     <div>
       <Header />
+      <FormLogin />
     </div>
   );
 };
