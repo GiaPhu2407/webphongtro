740a7ec42d82fd99b5ba36b8736a537ce16b70e1
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-19 22:46:52 +0700

    19/09/2024

---
+++ app/components/ForgotPass/FormForgotPass.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import React from "react";
+
+const FormRegister = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="bg-[#E4D3D3] w-[1073px] h-[923px] relative top-10 left-[140px] rounded-xl mr-64">
+        <p className="absolute top-12 left-20 text-2xl font-serif font-normal">
+          Đăng Ký
+        </p>
+        <form className="absolute top-40 left-24 ">
+          <p className="mt-2  text-xl">Tên tài khoản</p>
+          <input
+            type="text"
+            placeholder="Tên tài khoản"
+            className="mt-7 py-1 w-[400px]"
+          />
+          <br />
+          <p className="mt-10 text-xl">Mật khẩu mới</p>
+          <input
+            type="password"
+            placeholder="Mật khẩu"
+            className=" mt-6 py-1 w-[400px]"
+          />
+          <br />
+          <p className="mt-10  text-xl">Nhập lại mật khẩu</p>
+          <input
+            type="number"
+            placeholder="Số Điện Thoại"
+            className=" mt-6 py-1 w-[400px]"
+          />
+          <br />
+          
+
+          <button
+            type="submit"
+            className="absolute bg-[#0071C2] px-96 top-[650px] left-11 py-2 rounded-2xl whitespace-nowrap text-white"
+          >
+            Đăng Nhập
+          </button>
+          <div className=" absolute top-[300px] ml-12 whitespace-nowrap flex">
+            <Link
+              href={""}
+              className="absolute top-[400px] left-40 text-[#00C2FF] underline"
+            >
+              Quên mật khẩu?
+            </Link>
+            <Link href={""} className="absolute top-[400px]">
+              Bạn đã có tài khoản?
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FormRegister;
