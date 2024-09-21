e967134647a4c97e3bc73dc62fe07c929bc28367
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-21 20:41:24 +0700

    21/09/2024

--- app/components/ForgotPass/FormForgotPass.tsx
+++ app/components/ForgotPass/FormForgotPass.tsx
@@ -1,24 +1,6 @@
-"use client";
-// pages/forgot-password.js
-import { useState } from "react";
 import Link from "next/link";
 
 const ForgotPassword = () => {
-  const [email, setEmail] = useState("");
-
-  const handleSubmit = async (e: any) => {
-    e.preventDefault(); // Prevents the form from refreshing the page
-
-    try {
-      // Replace the console log with your email submission logic, such as an API call
-      console.log("Email submitted:", email);
-
-      // You can add additional handling logic here (e.g., sending a POST request)
-    } catch (error) {
-      console.error("An error occurred during submission:", error);
-    }
-  };
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="relative py-1 pr-4 mt-5 sm:max-w-3xl bg-gradient-to-r from-blue-500 to-purple-500 rounded-3xl sm:mx-auto w-full">
@@ -26,7 +8,7 @@ const ForgotPassword = () => {
           <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">
             Quên Mật Khẩu
           </h2>
-          <form onSubmit={handleSubmit}>
+          <form>
             <label
               htmlFor="email"
               className="block text-sm font-medium text-gray-700"
@@ -38,9 +20,6 @@ const ForgotPassword = () => {
               id="email"
               name="email"
               placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
               className="mt-1 p-3 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
             />
             <Link
@@ -62,6 +41,3 @@ const ForgotPassword = () => {
 };
 
 export default ForgotPassword;
-
-{
-}
