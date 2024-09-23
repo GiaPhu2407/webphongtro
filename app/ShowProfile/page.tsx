f2b7c7e52bf1008b4bf6f19a6a6f5a2ffce6d061
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-23 22:24:48 +0700

    23/09/2024

---
+++ app/ShowProfile/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Profile/Header'
+import Content from '../components/Profile/Content'
+import Footer from '../components/Profile/Footer'
+const page = () => {
+  return (
+    <div>
+        <Header />
+        <Content />
+        <Footer />
+  
+    </div>
+  )
+}
+
+export default page
---
+++ app/components/Profile/Header.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { VscAccount } from "react-icons/vsc";
+import { GrLogin } from "react-icons/gr";
+
+import { FaRegHeart } from "react-icons/fa";
+import Link from "next/link";
+const Header = () => {
+  return (
+    <div>
+      <div className="flex ml-10 gap-20 mt-1 justify-center items-center">
+        <div>
+          <a href="" className="text-2xl font-bold">
+            <span className="text-[#1C30E7] ">PhongTroSinhVien</span>
+            <span className="text-[#E71C1C]">Top</span>
+            <span className="text-[#8D90AC]">.com</span>
+          </a>
+        </div>
+        <div className="flex ml-36">
+          <FaRegHeart className="mt-[3px]" />
+          <p className="ml-1">Yêu thích</p>
+        </div>
+        <div className="flex">
+          <VscAccount className="mt-[3px]" />
+          <Link className="ml-1" href={"/ShowLogin"}>
+            Đăng nhập
+          </Link>
+        </div>
+        <div className="flex ">
+          <GrLogin className="mt-1" />
+          <a href="" className="ml-1">
+            Đăng ký
+          </a>
+        </div>
+        <div>
+          <button className="bg-[#F73859] px-5 py-2 rounded-xl hover:bg-red-700">
+            Đăng tin +
+          </button>
+        </div>
+      </div>
+      {/* Nvarbar */}
+      <div
+        style={{ backgroundColor: "#1266DD" }}
+        className=" w-full h-10 mt-2 flex justify-center items-center sticky top-0 z-50 "
+      >
+        <div className="flex  content-center gap-12">
+          <a href="" className=" text-white hover:bg-red-600 py-2 px-2">
+            Trang Chủ
+          </a>
+          <a href="" className=" text-white hover:bg-red-600 py-2 px-2">
+            Cho thuê phòng trọ
+          </a>
+          <a href="" className=" text-white hover:bg-red-600 py-2 px-2">
+            Giới thiệu{" "}
+          </a>
+          <a href="" className=" text-white hover:bg-red-600 py-2 px-2">
+            Tin Tức
+          </a>
+          <a href="" className=" text-white hover:bg-red-600 py-2 px-2">
+            Liên hệ
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
