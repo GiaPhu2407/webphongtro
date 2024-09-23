21cd2ebecc7ef44312e6c30ceea494fb8d239faf
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-23 21:41:34 +0700

    23/09/2024

--- app/ShowRoomForRent/page.tsx
+++ app/ShowRoomForRent/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/RoomForRent/Header";
 import CustomCarousel from "../components/RoomForRent/Carousel";
 import Content from "../components/RoomForRent/Content";
+import Footer from "../components/RoomForRent/Footer";
 const page = () => {
   return (
     <div>
       <Header />
       <CustomCarousel />
       <Content />
+      <Footer />
     </div>
   );
 };
