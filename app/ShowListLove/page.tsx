784268629ea949a765b2d76dc135f47c7ac13d5f
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-27 22:51:15 +0700

    27/09/2024

---
+++ app/ShowListLove/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/ListLiKe/Header";
+import Footer from "../components/ListLiKe/Footer";
+import Content from "../components/ListLiKe/Content";
+const page = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default page;
