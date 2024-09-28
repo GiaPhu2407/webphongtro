7974483b0d24132717c10b342dd120a0eb3bfb75
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-28 10:42:30 +0700

    28/09/2024

--- app/ShowListLove/page.tsx
+++ app/ShowListLove/page.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "../components/ListLiKe/Header";
 import Footer from "../components/ListLiKe/Footer";
 import Content from "../components/ListLiKe/Content";
+import List from "../components/ListLiKe/List";
 const page = () => {
   return (
     <div>
       <Header />
+      <List />
       <Content />
       <Footer />
     </div>
