9b4115ddee593a8557f37f8431180a1eb3d5cdf3
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-10-06 22:49:25 +0700

    06/10/2024

--- app/components/RoomForRent/Content1.tsx
+++ app/components/RoomForRent/Content1.tsx
@@ -3,12 +3,16 @@ import React, { useState } from "react";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { FaPhoneVolume } from "react-icons/fa6";
 import { FaFacebookMessenger } from "react-icons/fa";
-
 import { FaRegUser } from "react-icons/fa6";
+
 const Content1 = () => {
   const [isModalOpen, setModalOpen] = useState(false); // State để quản lý hiển thị modal
   const openModal = () => {
     setModalOpen(true); // Mở modal
+  };
+
+  const closeModal = () => {
+    setModalOpen(false); // Đóng modal
   };
 
   return (
@@ -43,13 +47,12 @@ const Content1 = () => {
                   <div className="py-10 text-center">
                     <div className="flex bg-[#39D336] h-10 mx-11 justify-center items-center rounded-xl transform transition-transform duration-300 cursor-pointer hover:scale-110 ">
                       <FaPhoneVolume />
-
-                      <a href="" className="w-40 h-20 mt-14  ">
+                      <a href="" className="w-40 h-20 mt-14">
                         0333924997
                       </a>
                     </div>
                     <p className="py-5">Hoặc</p>
-                    <div className=" bg-[#D9D9D9] flex h-10  mx-11 justify-center items-center rounded-xl transform transition-transform duration-300 cursor-pointer hover:scale-110 ">
+                    <div className="bg-[#D9D9D9] flex h-10  mx-11 justify-center items-center rounded-xl transform transition-transform duration-300 cursor-pointer hover:scale-110 ">
                       <FaFacebookMessenger />
                       <a href="" className="w-40 h-20 mt-14 whitespace-nowrap ">
                         Nhắn tin liên hệ
@@ -58,43 +61,42 @@ const Content1 = () => {
                   </div>
                 </div>
                 <form method="dialog" className="modal-backdrop">
-                  <button>close</button>
+                  <button onClick={closeModal}>Đóng</button>{" "}
+                  {/* Sử dụng closeModal để đóng modal */}
                 </form>
               </dialog>
             )}
           </div>
         </div>
         {/* ở dưới */}
-
         <div className="ml-10">
           <p className="text-xl mt-2">Thông tin mô tả</p>
-          <p className=" mt-2">
+          <p className="mt-2">
             Chính chủ cho thuê phòng trọ 25-30 m2, còn một phòng master của căn
             nhà do khách có việc mới trả phòng.
           </p>
-          <p className=" mt-2">
+          <p className="mt-2">
             Nội thất đầy đủ (tủ Hoàng Anh Gia Lai, nệm 1.8 m, tủ lạnh, bếp, WC
-            riêng) trên đường Tiểu La, gần chợ gần các chợ, trường trường Đại
-            Học Ngoại Ngữ, Đông Á, Kiến Trúc. Gần các trường tiểu học Núi Thành,
-            Phan Đăng Lưu, THCS Tây Sơn, Nguyễn khuyến, THPT Nguyễn Hiền....
+            riêng) trên đường Tiểu La, gần chợ gần các chợ, trường Đại Học Ngoại
+            Ngữ, Đông Á, Kiến Trúc. Gần các trường tiểu học Núi Thành, Phan Đăng
+            Lưu, THCS Tây Sơn, Nguyễn Khuyến, THPT Nguyễn Hiền...
           </p>
-          <p className=" mt-2">Khu dân cư an ninh tốt.</p>
-          <p className=" mt-2">Nhà thoáng mát có cửa sổ và ban công.</p>
-          <p className=" mt-2">Giá thuê: 3,3 triệu đến 3.5 triệu</p>
-          <p className=" mt-2">Liên hệ: 0982303649</p>
+          <p className="mt-2">Khu dân cư an ninh tốt.</p>
+          <p className="mt-2">Nhà thoáng mát có cửa sổ và ban công.</p>
+          <p className="mt-2">Giá thuê: 3,3 triệu đến 3.5 triệu</p>
+          <p className="mt-2">Liên hệ: 0982303649</p>
           <p className="mt-4">Bản đồ</p>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3834.399470944275!2d108.21386127417912!3d16.04474724009479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314219c106b37bd7%3A0x8948e865bd9c8b05!2zMTQxIFRp4buDdSBMYSwgSG_DoCBDxrDhu51uZyBC4bqvYywgSOG6o2kgQ2jDonUsIMSQw6AgTuG6tW5nIDU1MDAwMCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1727101773643!5m2!1svi!2s"
             width="600"
             height="450"
             style={{ border: 0 }}
-            // allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
             className="w-full"
           />
         </div>
-        <div className="w-full  mx-auto p-4 ">
+        <div className="w-full mx-auto p-4 ">
           {/* Input Section */}
           <div className="flex items-center mb-4">
             <FaRegUser />
@@ -106,7 +108,7 @@ const Content1 = () => {
           </div>
 
           {/* Divider */}
-          <div className="border-t h-1 w-full border-[#000000]  mt-5"></div>
+          <div className="border-t h-1 w-full border-[#000000] mt-5"></div>
           <button className="ml-[1000px] mt-2 px-4 py-2 bg-[#00C2FF] text-black font-semibold rounded-lg w-[147px]">
             Bình Luận
           </button>
